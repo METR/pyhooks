c8f20784daac4c05c4f4c1f9e07f0926a7585d4a
Ben West <Xodarap@users.noreply.github.com>
2024-06-15 18:15:14 -0700

    r

--- pyhooks/__init__.py
+++ pyhooks/__init__.py
@@ -240,7 +240,7 @@ class Hooks(BaseModel):
     # Don't wait for log, action, observation, frameStart, or frameEnd. Instead, run them in the background
 
     def log(self, *content: Any):
-        return self.log_with_attributes({'style': 'color:red'}, content)
+        return self.log_with_attributes(None, content)
     
     def log_with_attributes(self, attributes: dict, *content: Any):
         entry = self.make_trace_entry({"content": content, "attributes": attributes})
