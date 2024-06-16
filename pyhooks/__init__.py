4ca827baf8ad6347b397e5bd9f362dfde5c30608
Ben West <Xodarap@users.noreply.github.com>
2024-06-15 17:23:57 -0700

    i

--- pyhooks/__init__.py
+++ pyhooks/__init__.py
@@ -247,8 +247,8 @@ class Hooks(BaseModel):
         entry = self.make_trace_entry({"content": content})
         # entry = self.make_trace_entry({"content": content + 'added'})
         asyncio.create_task(trpc_server_request("mutation", "log", entry))
-        entry = self.make_trace_entry({"content": content, "attributes": attributes})
-        asyncio.create_task(trpc_server_request("mutation", "log", entry))
+        # entry = self.make_trace_entry({"content": content, "attributes": attributes})
+        # asyncio.create_task(trpc_server_request("mutation", "log", entry))
 
     def log_image(self, image_url: str, description: str | None = None):
         entry = self.make_trace_entry(
